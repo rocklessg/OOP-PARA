cce60b50fc24f66c3340c30347b78d71a2cc9e36
rocklessg <abdulhafiz.suleiman@decagon.dev>
2023-05-25 18:04:26 +0100

    Inheritance and polymorphism

---
+++ Model/LineOfCreditAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class LineOfCreditAccount : BankAccount
+    {
+
+        public LineOfCreditAccount(string accountOwner, decimal initialDeposite) : base(accountOwner, initialDeposite)
+        {
+
+        }
+
+        public override void PerformMonthEndTransactions()
+        {
+            if (Balance < 0)
+            {
+                // Negate the balance to get a positive interest charge
+                decimal interest = -Balance * 0.07m;
+                MakeWithdrawal(interest, DateTime.UtcNow, "Charge Monthly interest");
+            }
+        }
+    }
+}
