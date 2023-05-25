7817c34e8526d4532805c56ddaafcc72c168e5ec
rocklessg <abdulhafiz.suleiman@decagon.dev>
2023-05-25 15:12:36 +0100

    Add project files.

---
+++ Model/BankAccount.cs
@@ -0,0 +1,87 @@
+﻿using System.Text;
+
+namespace Model
+{
+    public class BankAccount
+    {
+        private static int accountNumberSeed = 1234567890;
+
+        public string  AccountNumber{ get; set; }
+        public string AccountOwner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        public BankAccount(string accountOwner, decimal initialBalance)
+        {
+            AccountOwner = accountOwner;
+            AccountNumber = accountNumberSeed.ToString();
+            accountNumberSeed++;
+            MakeDeposite(initialBalance, DateTime.UtcNow, "Initial balance");
+        }
+
+        private List<Transaction> allTransactions = new();
+
+        public void MakeDeposite(decimal amount, DateTime date, string narration)
+        {
+            try
+            {
+                if (amount <= 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(amount), "Deposite amount must be positive");
+                }
+
+                var deposite = new Transaction(amount, date, narration);
+                allTransactions.Add(deposite);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string narration)
+        {
+            try
+            {
+                if (amount <= 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(amount), "Deposite amount must be positive");
+                }
+                if (Balance - amount < 0)
+                {
+                    throw new InvalidOperationException("Insufficient fund");
+                }
+                var withdraw = new Transaction(-amount, date, narration);
+                allTransactions.Add(withdraw);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+            report.AppendLine("Date\t\tAmount\tBalance");
+            decimal balance = 0;
+
+            foreach (var transaction in allTransactions)
+            {
+                balance += transaction.Amount;
+                report.AppendLine($"{transaction.Date.ToShortDateString()}\t{transaction.Amount}\t{transaction.Narration}");
+            }
+            return report.ToString();
+        }
+    }
+}
---
+++ OopPara/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Model;
+
+Console.WriteLine("Hello, World!");
+
+var account = new BankAccount("Abdulhafiz", 2000);
+var account1 = new BankAccount("Abdulhafiz", 10000);
+
+Console.WriteLine($"Welcome {account.AccountOwner}, Your account number is {account.AccountNumber} and you have {account.Balance} initial balance");
+Console.WriteLine($"Welcome {account.AccountOwner}, Your account number is {account1.AccountNumber} and you have {account1.Balance} initial balance");
+
+account.MakeDeposite(10000, DateTime.UtcNow, "Food fund");
+Console.WriteLine(account.Balance);
+account.MakeWithdrawal(1000, DateTime.UtcNow, "Passport fund");
+Console.WriteLine(account.Balance);
+
+Console.WriteLine(account.GetAccountHistory());
