311692f0a81bf4e4595f5d30f71ada7cf8d01604
rocklessg <abdulhafiz.suleiman@decagon.dev>
2023-06-08 12:12:05 +0100

    Interface segregation principle violation

---
+++ SolidPrinciple/InterfaceSegregation/Animal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciple.InterfaceSegregation
+{
+    public class Animal : ILivingthing
+    {
+        public void Feed()
+        {
+            Console.WriteLine("All animals can feed");
+        }
+
+        public bool GiveBirthToYoungerOnesAlive(string type)
+        {
+            if (string.IsNullOrEmpty(type))
+                return false;
+
+            if (type == "Mammals")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string Grow()
+        {
+            return "All animals grow";
+        }
+
+        public string Speak(string nature)
+        {
+            if (string.IsNullOrEmpty(nature))
+            {
+                return "Invalid request";
+            }
+
+            if (nature == "Homonsapient")
+            {
+                return $"{nature} can speak";
+            }
+            return $"{nature} can not speak";
+        }
+    }
+}
